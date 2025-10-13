2d3fcc904539978996ab5f3cdd564db3e7222127
Ali <alialnaji2020@gmail.com>
2025-10-14 00:24:42 +0300

    inergration

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+Route::get('/collection', function () {
+    return view('Collection');
+})->name('collection');
+
+Route::get('/product', function () {
+    return view('product-fullwidth');
+})->name('product');
